248e8924664f7a072243fef836ad06e813b205ff
3rwinn <erwinngrebo@outlook.com>
2019-09-21 20:23:20 +0000

    Ajout du module twitter

--- pages/views.py
+++ pages/views.py
@@ -7,11 +7,26 @@ from django.http import HttpResponse
 from django.contrib import messages
 from publications.models import Publication
 
+import tweepy
+
+# Authenticate to twitter
+auth = tweepy.OAuthHandler("Cfa5MfJunGt68CesNSamX6CeA", 
+    "Fo4WlqSe3jpw3cGwztQF0hejjVRYr0FbC3OMMp8tSkfV19O61Q")
+auth.set_access_token("200647729-yEXnV9BcJr881gyUQ8uNOcOSmXROQYQy1vmScFGk", 
+    "On8hwicGCgP30LvpWMtmSgOdowggsSOfhqWjYbFWvgCuW")
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True,
+    wait_on_rate_limit_notify=True)
+
+# Get last n tweets
+tweets = api.user_timeline("amkpayne", count=10)
+
 
 def index(request):
     publications = Publication.objects.order_by(
         '-publication_date').filter(est_visible=True)
-    context = {'publications': publications}
+    context = {'publications': publications, 'tweets': tweets}
     return render(request, 'pages/accueil.html', context)
 
 
